3bfa1da724d6fa1a1029f40aed550e48ad32ed0b
luxetverit <replaysoccer@naver.com>
2022-07-11 15:27:10 +0900

    commit

---
+++ exchangeList.js
@@ -0,0 +1,45 @@
+const axios = require('axios')
+const cheerio = require('cheerio')
+const iconv = require('iconv-lite')
+
+const getExchange = async () => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let html = await axios({
+                method: 'get',
+                url: 'https://finance.naver.com/marketindex/exchangeList.naver',
+                responseEncoding: 'binary',
+            })
+            let exchangeList = []
+            const contents = iconv.decode(html.data, 'euc-kr').toString()
+            const $ = cheerio.load(contents)
+            const $bodyList = $('div.tbl_area tbody tr') //.find('tbody tr')
+            //console.log($bodyList)
+            $bodyList.map(function (i, elem) {
+                //var ele = $(this).text()
+                let nation = String(
+                    $(elem)
+                        .find('td:nth-of-type(1)')
+                        .text()
+                        .replace(/\t/gi, '')
+                        .replace(/\n/g, '')
+                )
+                let rate = String($(elem).find('td:nth-of-type(2)').text())
+                exchangeList.push({
+                    nation,
+                    rate,
+                })
+            })
+            resolve(exchangeList)
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
+
+async function getitem() {
+    var result = await getExchange()
+    console.log(JSON.stringify(result))
+}
+
+getitem()
